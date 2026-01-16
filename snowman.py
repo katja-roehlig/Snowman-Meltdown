e8d863d4c35fd504812c6dd9393d3f48be15d1c7
Katja Röhlig <katja-roehlig@mail.de>
2026-01-16 13:28:39 +0100

    refactor play_game function

--- snowman.py
+++ snowman.py
@@ -14,44 +14,51 @@ def display_game_state(mistakes):
     print(STAGES[mistakes])
 
 
-def display_word_state(printed_word, index_list, user_char):
+def find_word_state(printed_word, index_list, user_char):
     list_word = printed_word.split(" ")
     if not index_list:
         return printed_word
     for index in index_list:
         list_word[index] = user_char
-    printed_word = " ".join(list_word)
-    print(printed_word)
+    new_printed_word = " ".join(list_word)
+    return new_printed_word
 
 
-display_word_state("_ _ _ _ _ _", [1, 3, 5], "a")
-
-
-def find_occurences(secret_word, user_char):
-    index_list = [i for i, char in enumerate(secret_word) if char == user_char]
+def find_occurences(word, user_char):
+    index_list = [i for i, char in enumerate(word) if char == user_char]
     return index_list
 
 
-print(find_occurences("canada", "x"))
+def prepare_game(secret_word):
+    print("Welcome to Snowman Meltdown!")
+    print("Secret word selected: " + secret_word)
+    print(STAGES[0])
+    length_secret_word = len(secret_word)
+    printed_word = "_ " * length_secret_word
+    print(f"Word: {printed_word}")
+    return printed_word
 
 
-# def play_game():
-#     secret_word = get_random_word()
-#     print("Welcome to Snowman Meltdown!")
-#     print("Secret word selected: " + secret_word)  # for testing, later remove this line
-#     print(STAGES[0])
-#     length_secret_word = len(secret_word)
-#     printed_word = "_ " * length_secret_word
-#     print(f"Word: {printed_word}")
-#     guess = input("\nGuess a letter: ").lower()
-#     # print("You guessed:", guess)
-#     mistakes = 0
-#     index = secret_word.find(guess)
-#     if index == -1:
-#         mistakes += 1
-#     print(STAGES[mistakes])
-#     print(f"Word: {'_ '*index}{guess} {'_ '* (length_secret_word - index -1)}")
+def play_game():
+    secret_word = get_random_word()
+    printed_word = prepare_game(secret_word)
+    mistakes = 0
+    while True:
+        user_char = input("\nGuess a letter: ").lower()
+        index_list = find_occurences(secret_word, user_char)
+        if not index_list:
+            mistakes += 1
+        display_game_state(mistakes)
+        printed_word = find_word_state(printed_word, index_list, user_char)
+
+        print(f"Word: {printed_word}")
+        if mistakes == 3:
+            print(f"Game over! The word was {secret_word}")
+            break
+        if printed_word.replace(" ", "") == secret_word:
+            print("\nYou win! 🎉 \nThats the right word!")
+            break
 
 
-# if __name__ == "__main__":
-#     play_game()
+if __name__ == "__main__":
+    play_game()
