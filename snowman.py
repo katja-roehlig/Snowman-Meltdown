1cc885eb5b6890ab98a87ba02610db7f1bf92b99
Katja Röhlig <katja-roehlig@mail.de>
2026-01-16 11:36:31 +0100

    implement first run

--- snowman.py
+++ snowman.py
@@ -1,4 +1,5 @@
 import random
+from snowman_stages import STAGES
 
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
@@ -9,15 +10,30 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    pass
+
+
+def find_occurences(secret_word):
+    pass
+
+
 def play_game():
     secret_word = get_random_word()
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)  # for testing, later remove this line
-
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
+    print(STAGES[0])
+    length_secret_word = len(secret_word)
+    printed_word = "_ " * length_secret_word
+    print(f"Word: {printed_word}")
     guess = input("\nGuess a letter: ").lower()
-    print("You guessed:", guess)
+    # print("You guessed:", guess)
+    mistakes = 0
+    index = secret_word.find(guess)
+    if index == -1:
+        mistakes += 1
+    print(STAGES[mistakes])
+    print(f"Word: {'_ '*index}{guess} {'_ '* (length_secret_word - index -1)}")
 
 
 if __name__ == "__main__":
