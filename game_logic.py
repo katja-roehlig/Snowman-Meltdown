bf4156f4174eba30ee644bdc7743e6cccad7fb4a
Katja Röhlig <katja-roehlig@mail.de>
2026-01-16 13:57:34 +0100

    validate user_input, add docstrings

--- game_logic.py
+++ game_logic.py
@@ -10,26 +10,25 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
-def display_game_state(mistakes):
-    print(STAGES[mistakes])
-
-
-def find_word_state(printed_word, index_list, user_char):
-    list_word = printed_word.split(" ")
-    if not index_list:
-        return printed_word
-    for index in index_list:
-        list_word[index] = user_char
-    new_printed_word = " ".join(list_word)
-    return new_printed_word
-
-
-def find_occurences(word, user_char):
-    index_list = [i for i, char in enumerate(word) if char == user_char]
-    return index_list
+def get_user_input():
+    """
+    validates user_input
+    :return user_input
+    """
+    user_input = input("\nGuess a letter: ").lower()
+    while True:
+        if len(user_input) == 1 and user_input.isalpha():
+            return user_input
+        else:
+            print("That was not a valid input!")
 
 
 def prepare_game(secret_word):
+    """
+    prints the initial display
+    :param secret_word
+    :return printed_word
+    """
     print("Welcome to Snowman Meltdown!")
     print("Secret word selected: " + secret_word)
     print(STAGES[0])
@@ -39,12 +38,51 @@ def prepare_game(secret_word):
     return printed_word
 
 
+def find_occurences(word, user_char):
+    """
+    Finds out how often and where a letter appears in a word and stores the indices in a list
+    :param word
+    :param user_char
+    :return index_list
+    """
+    index_list = [i for i, char in enumerate(word) if char == user_char]
+    return index_list
+
+
+def display_game_state(mistakes):
+    """
+    printed the appearance of the snowman depending on the mistakes made
+    """
+    print(STAGES[mistakes])
+
+
+def find_word_state(printed_word, index_list, user_char):
+    """
+    inserts the letter guessed by the user in the correct positions in the word
+
+    :param printed_word: the current word with any letters already inserted
+    :param index_list: Beschreibung
+    :param user_char: Beschreibung
+    :returns the new word with the newly added letters
+    """
+    list_word = printed_word.split(" ")
+    if not index_list:
+        return printed_word
+    for index in index_list:
+        list_word[index] = user_char
+    new_printed_word = " ".join(list_word)
+    return new_printed_word
+
+
 def play_game():
+    """
+    the framework of the game, guides the user through the game
+    """
     secret_word = get_random_word()
     printed_word = prepare_game(secret_word)
     mistakes = 0
     while True:
-        user_char = input("\nGuess a letter: ").lower()
+        user_char = get_user_input()
         index_list = find_occurences(secret_word, user_char)
         if not index_list:
             mistakes += 1
